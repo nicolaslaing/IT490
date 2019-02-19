093ecc157bebce1529bd2408837fee623e3d4ac5
Nicolas Laing <nicolaslaing@outlook.com>
2019-02-19 12:05:35 -0500

    Login

---
+++ src/components/Login.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="login">
+    <h1>Music Repeater</h1>
+    <input id="username" type="text" v-model.lazy="login.username" placeholder="Enter Username"><br>
+    <input id="password" type="password" v-model.lazy="login.password" placeholder="Enter Password"><br>
+    <button id="submit" type="button" name="Submit" v-on:click.stop.prevent="login">Login</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      login: {
+        username: '',
+        password: '',
+      }
+    }
+  },
+  created(){
+
+  },
+  methods: {
+    login(){
+      let self = this
+      this.axios.post('', this.login)
+      .then(function(response){
+        self.$router.push({
+          name: 'Dashboard'
+        })
+      })
+      .catch(function(error){
+        console.log(error)
+      })
+    },
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Login from '@/components/Login'
 import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
@@ -8,6 +9,11 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/helloworld',
       name: 'HelloWorld',
       component: HelloWorld
     }
